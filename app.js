299a84faacae7c06eede72fd62eb0abc986e908d
Mirio1010 <miguelangelortega2021@gmail.com>
2026-01-06 18:19:41 -0500

    added logic

--- app.js
+++ app.js
@@ -2,83 +2,83 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    img: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: "susan smith",
+    job: "web developer",
+    img: "https://www.course-api.com/images/people/person-1.jpeg",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    img: 'https://www.course-api.com/images/people/person-2.jpeg',
-    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://www.course-api.com/images/people/person-2.jpeg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    img: 'https://www.course-api.com/images/people/person-4.jpeg',
-    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    name: "peter jones",
+    job: "intern",
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
-    img: 'https://www.course-api.com/images/people/person-3.jpeg',
-    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
-// select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+//  <article class="review">
+//         <div class="img-container">
+//           <img src="person-1.jpeg" id="person-img" alt="" />
+//         </div>
+//         <h4 id="author">sara jones</h4>
+//         <p id="job">UX designer</p>
+//         <p id="info">
+//           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil,
+//           soluta. Blanditiis aut porro, impedit earum et laboriosam tenetur!
+//           Tenetur aspernatur quis id? Eius sint voluptates cum laborum maxime
+//           adipisci fugiat. Corrupti officia labore iure. Ex aliquam ipsam ea
+//           hic fuga!
+//         </p>
 
-// set starting item
-let currentItem = 0;
+let img = document.getElementById("person-img");
+let author = document.getElementById("author");
+let job = document.getElementById("job");
+let info = document.getElementById("info");
 
-// load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+let buttons = document.querySelector(".button-container");
+let arr = [...buttons.children];
+
+let index = 0;
+
+arr.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    let classList = e.currentTarget.classList;
+    if (classList.contains("prev-btn")) {
+      index === 0 ? (index = reviews.length - 1) : index--;
+      showReview(index);
+    } else {
+      index === reviews.length - 1 ? (index = 0) : index++;
+      showReview(index);
+    }
+  });
 });
 
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+let random = document.querySelector(".random-btn");
+
+random.addEventListener("click", function () {
+  index = Math.floor(Math.random() * reviews.length);
+  console.log(index);
+
+  showReview(index);
+});
+
+function showReview(index) {
+  img.src = reviews[index].img;
+  author.textContent = reviews[index].name;
+  job.textContent = reviews[index].job;
+  info.textContent = reviews[index].text;
 }
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
-
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
